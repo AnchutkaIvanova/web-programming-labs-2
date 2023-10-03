bfb2644d9ee3f65b323033153f1a308e678a6b2b
Ivanova Anna <ivanova@.com>
2023-10-03 22:40:32 +0700

    Правки всего

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
-from flask import Flask, redirect, url_for
+from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/index")
 def start():
-     return redirect("/menu", code=302)
-
-@app.route("/menu")
-def menu():
-    return"""
+    return """
 <!doctype html>
 <html>
      <head>
@@ -17,32 +13,37 @@ def menu():
      <body>
             <header>
             НГТУ, ФБ, WEB-программирование, часть 2. Спиок лабораторных
-            <ol>
-               <a href="/lab1" target="_blank" >Первая лабораторная</a>
-             </ol>
+            </header>
+            <h1>web-сервер на flask</h1>
+            <footer>
             &copy; Иванова Анна Алексеевна, ФБИ-14, 3 курс, 2023
             </footer>
         </body>
 </html>
 """
-@app.route('/lab1/oak')
-def oak():
-    return'''
+@app.route("/lab1")
+def lab1():
+    return """
 <!doctype html>
 <html>
- <head>
-        <title>Иванова Анна Алексеевна, лабораторная 1</title>
-    </head>
+     <head>
+           <title>Иванова Анна Алексеевна, лабораторная 1</title>
+     </head>
      <body>
-     <link rel="stylesheet" href="'''+ url_for('static', filename='lab1.css') + '''">
-     <header>
-     НГТУ, ФБ, Лабораторная работа 1
-     </header>
-          <h1>Дуб</h1>
-          <img src=''' + url_for('static', filename='oak.jpg') + '''>
-    <footer>
-    &copy; Иванова Анна, ФБИ-14, 3 курс, 2023
-    </footer>
-    </body>
+            <header>
+            НГТУ, ФБ, Лабораторная работа 1
+            </header>
+            <h1>web-сервер на flask</h1>
+              <menu>Первая лабораторная</menu>
+            <p>
+            Flask - фреймворк для создания веб-приложений на языке программирования Python, использующий набор инструментов
+            Werkzeug, а также шаблонизатор Jinja2. Относится к категории так называемых микрофреймворков минималистичных каркасов
+            веб-приложений, сознательно предоставляющих лишь самые базовые возможности.
+            </p>
+            
+            <footer>
+            &copy; Анна Алексеевна, ФБИ-14, 3 курс, 2023
+            </footer>
+        </body>
 </html>
-'''
+"""
